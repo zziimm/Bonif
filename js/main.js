d377c0029b9afc6622b03442ec2bb5fed6f0f1d7
zziimm <wlalscjs30@naver.com>
2023-09-20 17:53:58 +0900

    02_day +02
    피드백 적용
    - 폰트x
    - nav x

--- js/main.js
+++ js/main.js
@@ -25,6 +25,15 @@ spyEls.forEach(function (spyEl) {
   .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
   .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
 });
+const spyElse = document.querySelectorAll('section .inner.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.7 // 화면의 80% 지점에서 보여짐 여부 감시(0~1사이 지정)
+  })
+  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
 
 // const name1 = document.querySelector('.main-contents .bon-name')
 // window.addEventListener('scroll', function () {
